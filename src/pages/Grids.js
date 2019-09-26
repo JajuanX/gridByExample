6225417a8de636eb445c46ab34cb5d9e479ec9ba
JajuanX <jajuan.burton@live.com>
2019-09-25 20:03:09 -0400

    Start Up

---
+++ src/pages/Grids.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+
+class GridByExample extends Component {
+
+
+  render() {
+  return(
+          <div className="container">
+            <div className="box O"> O </div>
+            <div className="box Heart"> ❤️ </div>
+            <div className="box W"> W </div>
+            <div className="box Y"> Y </div>
+            <div className="box I"> I </div>
+            <div className="box E"> E </div>
+            <div className="box D"> D </div>
+            <div className="box N"> N </div>
+            <div className="box C"> C </div>
+          </div>
+
+
+        )
+  }
+}
+
+export default GridByExample
